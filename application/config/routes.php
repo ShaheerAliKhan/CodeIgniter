8a5d2665fef4adca9c9cd93dfe6a1080f1cbb110
ShaheerAliKhan <bloodreservation@gmail.com>
2020-09-07 13:41:09 +0500

    Added CRUD functionality

--- application/config/routes.php
+++ application/config/routes.php
@@ -58,3 +58,7 @@ $route['stud/add'] = 'Stud_controller/add_student';
 $route['stud/add_view'] = 'Stud_controller/add_student_view'; 
 $route['stud/edit/(\d+)'] = 'Stud_controller/update_student_view/$1'; 
 $route['stud/delete/(\d+)'] = 'Stud_controller/delete_student/$1';
+
+$route['upload'] = 'Upload';
+
+$route['validation'] = 'Form';
